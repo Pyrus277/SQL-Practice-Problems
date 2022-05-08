2cde254f3d1c1ca4766bad80c3a9402977ee6502
Perry Fox <pyrus277@gmail.com>
2022-05-08 01:26:38 -0400

    Advanced problem set

--- Problems_32-39.sql
+++ Problems_32-39.sql
@@ -1,8 +1,9 @@
+-- (ctrl k + c to comment out)
+
 -- Problem 32
 -- Identify high-value customers as those who've made at least 1 order with a total 
 -- value (not including discount) equal to $10k or more. Only want to consider order
 -- placed in 2016
-
 
 SELECT 
 	c.CustomerID
@@ -19,6 +20,29 @@ ORDER BY TotalOrderAmount DESC
 ;
 
 -- Problem 33
+--The manager has changed his mind since the last problem.
+--He wants to define high-value customers differently. 
+--Instead of requiring that customers have at least one order totaling $10,000 or
+--more, high-value customers are those who have orders totaling $15,000 or more
+--in 2016. How would you change the query above to find this?
 
+SELECT 
+	c.CustomerID
+	,c.CompanyName
+	,SUM(od.UnitPrice * Quantity) as TotalOrderAmount
+FROM Customers c  
+	JOIN Orders o ON c.CustomerID = o.CustomerID
+	JOIN OrderDetails od ON o.OrderID = od.OrderID
+WHERE YEAR(o.OrderDate) = 2016 
+GROUP BY c.CustomerID, c.CompanyName
+HAVING SUM(od.UnitPrice * Quantity) >= 15000
+ORDER BY TotalOrderAmount DESC
+-- Removing order ID collapsed the groupings allowing us to see the total order amounts!
+;
 
+SELECT * FROM Customers;
+SELECT * FROM Orders;
+SELECT * FROM OrderDetails;
+
+-- Problem 34
 
